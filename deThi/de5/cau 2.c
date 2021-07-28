065b1bf20f09a17f39bc1a01eb5157e37d937e77
PhuongAnh <phamphuonganh.hvact@gmail.com>
2021-07-28 19:39:19 +0700

    tich hai ma tran

--- deThi/de5/cau 2.c
+++ deThi/de5/cau 2.c
@@ -2,80 +2,106 @@
 sau do tinh tinh tich ma tran c = a*b; in ba ma tran ra man hinh*/
 #include <stdio.h>
 
-void matranA(int a1[][100], int dong1, int cot1)
-{
 
-      for(int i=0; i<dong1; i++){
-        for(int j=0; j<cot1; j++){
-            scanf("%d",&a1[i][j]);
+void nhapMaTran(int a[][100], int dong, int cot)
+{
+    for(int i=0; i<dong; i++)
+    {
+        for(int j=0; j<cot; j++)
+        {
+            scanf("%d",&a[i][j]);
         }
-      }
-       printf("matran A:\n");
-      for(int i=0; i<dong1; i++){
-        for(int j=0; j<cot1; j++){
-            printf("%4d", a1[i][j]);
+    }
+}
+void hienthi(int a[][100], int dong, int cot)
+{
+    for(int i=0; i<dong; i++)
+    {
+        for(int j=0; j<cot; j++)
+        {
+            printf("%4d",a[i][j]);
         }
         printf("\n");
-      }
+    }
 
 
 }
-void matranB(int a2[][100], int dong2, int cot2)
+
+void tichHaiMaTran(int a1[][100], int a2[][100], int a3[][100], int dong, int cot)
 {
 
-      for(int i=0; i<dong2; i++){
-        for(int j=0; j<cot2; j++){
-            scanf("%d",&a2[i][j]);
-        }
-      }
-      printf("matran B:\n");
-      for(int i=0; i<dong2; i++){
-        for(int j=0; j<cot2; j++){
-            printf("%4d", a2[i][j]);
-        }
-        printf("\n");
-      }
-}
-
-void matranC(int a1[][100], int a2[][100], int a3[][100], int dong, int cot)
-{
-
-    for(int i=0; i<dong; i++){
-        for(int j=0; j<cot; j++){
+    for(int i=0; i<dong; i++)
+    {
+        for(int j=0; j<cot; j++)
+        {
             a3[i][j]=0;
-        for (int k=0; k<cot;k++){
-            a3[i][j]+= a1[i][k] * a2[k][j];
-        }
+            for (int k=0; k<cot; k++)
+            {
+                a3[i][j]+= a1[i][k] * a2[k][j];
+            }
         }
     }
 
-    printf("matran c: \n");
-    for(int i=0; i<dong; i++){
-        for(int j=0; j<cot; j++){
-            printf("%4d", a3[i][j]);
-        }
-        printf("\n");
-    }
 }
+void  menu()
+{
+    printf("1. Nhap hai ma tran\n");
+    printf("2. Tính tich hai ma tran\n");
+    printf("3. Hien thi ba ma tran\n");
+    printf("==============================\n");
 
+}
 int main()
 {
     int a1[100][100],a2[100][100], a3[100][100];
     int dong1, cot1;
-    printf("nhap so dong cot ma tran A: ");
-    scanf("%d%d", &dong1, &cot1);
-    matranA(a1, dong1, cot1);
     int dong2, cot2;
-    printf("nhap so dong cot ma tran B: ");
-    scanf("%d%d", &dong2, &cot2);
-    matranB(a2, dong2, cot2);
     int dong, cot;
-    if(cot1 == dong2 ){
-        matranC(a1,a2,a3,dong, cot);
+
+    while(1)
+    {
+        menu();
+        int c;
+        scanf("%d",&c);
+        switch(c)
+        {
+        case 1:
+            printf("nhap so dong cot ma tran A: ");
+            scanf("%d%d", &dong1, &cot1);
+            nhapMaTran(a1,dong1,cot1);
+            printf("\nnhap so dong cot ma tran B: ");
+            scanf("%d%d", &dong2, &cot2);
+            nhapMaTran(a2,dong2,cot2);
+            break;
+        case 2:
+            if(cot1 == dong2 )
+            {
+                tichHaiMaTran(a1,a2,a3,dong1, cot1);
+            }
+            else
+            {
+                printf("hai ma tran khong the nhan");
+            }
+            break;
+        case 3:
+            printf("Ma tran A\n");
+            hienthi(a1,dong1,cot1);
+            printf("Ma tran b\n");
+            hienthi(a2,dong2,cot2);
+            printf("Ma tran C\n");
+            hienthi(a3,dong1,cot2);
+            break;
+        default:
+            printf("PA da quen lenh thoat chuong trinh");
+            break;
+        }
     }
-    else{
-        printf("hai ma tran khong the nhan");
-    }
+
+
+
+
+
+
     return 0;
 
 }
